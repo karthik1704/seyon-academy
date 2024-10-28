e5a69c5107d8b6aab855300d34a69ff5a2a1ed94
Karthi A <karthikthee7@gmail.com>
2024-10-28 11:56:04 +0530

    Refactor webinar date and time in constants.tsx Update dependencies in package.json

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,7 +1,7 @@
 
 
 
-export const WEBINAR_DATETIME = 'Oct 27, 2024 03 PM';
+export const WEBINAR_DATETIME = 'Nov 2, 2024 03 PM';
 export const WEBINAR_DAY = 'SUNDAY';
 export const WEBINAR_TIMING = '03 to 06 PM';
 export const WEBINAR_URL = 'https://imjo.in/e4Za96';
