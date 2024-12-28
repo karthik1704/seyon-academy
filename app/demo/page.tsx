20ea33144343952d3c06b23af9db02b092f64e6b
Karthi A <karthikthee7@gmail.com>
2024-12-28 22:33:02 +0530

    Update LiveClassPage component; adjust iframe source parameters and allow fullscreen mode

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -4,13 +4,13 @@ export default async function LiveClassPage() {
 
 
   return (
-    <div className="h-screen">
+    <div className="h-[80%] md:h-screen ">
       <div className="aspect-w-16 aspect-h-9 p-8 h-full">
         <iframe
           className="w-full h-full rounded-lg"
-          src={`${liveClass}?modestbranding=1&showinfo=0&rel=0&controls=0`}
+          src={`${liveClass}?modestbranding=1&showinfo=0&rel=0&controls=0&cc_load_policy=0&iv_load_policy=0`}
           title={"Demo  Class"}
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
           allowFullScreen
         ></iframe>
       </div>
